2533e28c30a06ce1f3f04720a2c7f5958b43b145
crisXand05 <cristianmndz009@gmail.com>
2023-03-06 19:22:26 -0600

    Key field hibernate

--- HibernateProject/src/com/chr/entities/Customer.java
+++ HibernateProject/src/com/chr/entities/Customer.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 public class Customer {
 	@Id
 	@Column(name="idCustomer")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int idCustomer;
 	
 	@Column(name="nameCustomer")
--- HibernateProject/src/com/chr/entities/MainTestConnection.java
+++ HibernateProject/src/com/chr/entities/MainTestConnection.java
@@ -9,17 +9,22 @@ public class MainTestConnection {
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class).buildSessionFactory();
 		Session session = factory.openSession();
 		try {
-			Customer cliente = new Customer("chr","mendoza", "san salvador");
+			Customer cliente = new Customer("Ricardo","Bautista", "El  Salvador");
 			session.beginTransaction();
 			session.save(cliente);
 			session.getTransaction().commit();
 			System.out.println("success save");
-			session.close();
-		
+			session.beginTransaction();
+			System.out.println("id: "+cliente.getIdCustomer());
+			Customer clienteInsertado = session.get(Customer.class, cliente.getIdCustomer());
+			System.out.println("registro: " + clienteInsertado);
 		}catch(Exception e) {
 			e.printStackTrace();
+			session.getTransaction().rollback();
 		}
 		finally {
+			session.close();
+
 			factory.close();
 		}
 	}
