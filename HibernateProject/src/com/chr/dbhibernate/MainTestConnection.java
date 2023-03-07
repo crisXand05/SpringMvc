294fb1e5574541261aab60681a247a17daa54f7b
crisXand05 <cristianmndz009@gmail.com>
2023-03-07 11:15:21 -0600

    Hql instructions crud

---
+++ HibernateProject/src/com/chr/dbhibernate/MainTestConnection.java
@@ -0,0 +1,34 @@
+package com.chr.dbhibernate;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.chr.entities.Customer;
+
+public class MainTestConnection {
+	public static void main(String args[]) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Customer.class).buildSessionFactory();
+		Session session = factory.openSession();
+		try {
+			Customer cliente = new Customer("vilma","Bautista", "El  Salvador");
+			session.beginTransaction();
+			session.save(cliente);
+			session.getTransaction().commit();
+			System.out.println("success save");
+			session.beginTransaction();
+			System.out.println("id: "+cliente.getIdCustomer());
+			Customer clienteInsertado = session.get(Customer.class, cliente.getIdCustomer());
+			System.out.println("registro: " + clienteInsertado);
+		}catch(Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		finally {
+			session.close();
+
+			factory.close();
+		}
+	}
+}
