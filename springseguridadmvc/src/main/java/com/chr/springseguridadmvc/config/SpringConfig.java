8ad255d0c83513e8a359669dbd8a6d734bc9ca83
crisXand05 <cristianmndz009@gmail.com>
2023-03-31 12:47:04 -0600

    Initializing Spring mvc Project config

---
+++ springseguridadmvc/src/main/java/com/chr/springseguridadmvc/config/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.chr.springseguridadmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan(basePackages= {"com.chr.springseguridadmvc"})
+public class SpringConfig {
+	
+}
